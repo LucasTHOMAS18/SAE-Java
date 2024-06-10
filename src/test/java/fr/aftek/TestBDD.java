9a204b7b3d135319b51f0791b56fac9f10b64980
Rickeur <raphael.cochet1234@outlook.fr>
2024-06-10 17:31:33 +0200

    Modification de creerBDD pour nouvelle BDD

--- src/test/java/fr/aftek/TestBDD.java
+++ src/test/java/fr/aftek/TestBDD.java
@@ -95,49 +95,72 @@ public class TestBDD extends TestCase {
 
     public void creerBDD() {
         String[] tableDrops = {
-            "DROP TABLE IF EXISTS ATHLETE;",
-            "DROP TABLE IF EXISTS EQUIPE;",
-            "DROP TABLE IF EXISTS PAYS;",
-            "DROP TABLE IF EXISTS SPORT;"
+            "DROP TABLE IF EXISTS ParticipeCollectif;",
+            "DROP TABLE IF EXISTS Participe;",
+            "DROP TABLE IF EXISTS Athlete;",
+            "DROP TABLE IF EXISTS Epreuve;",
+            "DROP TABLE IF EXISTS Equipe;",
+            "DROP TABLE IF EXISTS Sport;",
+            "DROP TABLE IF EXISTS Pays;"
         };
     
         String[] tableCreates = {
-            "CREATE TABLE PAYS(" +
-            "id_Pays INT PRIMARY KEY NOT NULL," +
-            "nom_Pays VARCHAR(25)" +
+            "CREATE TABLE Pays (" +
+            "nomPays VARCHAR(25) PRIMARY KEY NOT NULL" +
             ");",
     
-            "CREATE TABLE EQUIPE(" +
+            "CREATE TABLE Sport (" +
+            "idSport INT PRIMARY KEY NOT NULL," +
+            "nomSport VARCHAR(25)," +
+            "forcesRequis INT," +
+            "agiliteRequis INT," +
+            "enduranceRequis INT," +
+            "collectif BOOLEAN" +
+            ");",
+    
+            "CREATE TABLE Equipe (" +
             "idEquipe INT PRIMARY KEY NOT NULL," +
-            "nom_Equipe VARCHAR(25)," +
-            "id_Pays INT," +
-            "FOREIGN KEY (id_Pays) REFERENCES PAYS(id_Pays)" +
+            "nomEquipe VARCHAR(25)," +
+            "nomPays VARCHAR(25)," +
+            "FOREIGN KEY (nomPays) REFERENCES Pays(nomPays)" +
             ");",
     
-            "CREATE TABLE SPORT(" +
-            "id_Sport INT PRIMARY KEY NOT NULL," +
-            "nom_Sport VARCHAR(25)," +
-            "forces_requis INT," +
-            "agiliter_requis INT," +
-            "endurance_requis INT," +
-            "Collectif BOOLEAN" +
+            "CREATE TABLE Epreuve (" +
+            "idEpreuve INT PRIMARY KEY," +
+            "nomEpreuve VARCHAR(25)," +
+            "sexeEpreuve CHAR(1)," +
+            "idSport INT," +
+            "collective BOOLEAN," +
+            "FOREIGN KEY (idSport) REFERENCES Sport(idSport)" +
             ");",
     
-            "CREATE TABLE ATHLETE(" +
-            "id_Athlete INT PRIMARY KEY NOT NULL," +
-            "nom_Athlete VARCHAR(25)," +
-            "sexe CHAR," +
+            "CREATE TABLE Athlete (" +
+            "idAthlete INT PRIMARY KEY NOT NULL," +
+            "nomAthlete VARCHAR(25)," +
+            "sexe CHAR(1)," +
             "forces INT," +
-            "agiliter INT," +
+            "agilite INT," +
             "endurance INT," +
-            "id_Pays INT," +
-            "id_Equipe INT," +
-            "id_Sport INT," +
-            "nom_Epreuve VARCHAR(25)," +
-            "sexe_Epreuve CHAR," +
-            "FOREIGN KEY (id_Sport) REFERENCES SPORT(id_Sport)," +
-            "FOREIGN KEY (id_Equipe) REFERENCES EQUIPE(idEquipe)," +
-            "FOREIGN KEY (id_Pays) REFERENCES PAYS(id_Pays)" +
+            "nomPays VARCHAR(25)," +
+            "idEquipe INT," +
+            "FOREIGN KEY (idEquipe) REFERENCES Equipe(idEquipe)," +
+            "FOREIGN KEY (nomPays) REFERENCES Pays(nomPays)" +
+            ");",
+    
+            "CREATE TABLE Participe (" +
+            "idAthlete INT," +
+            "idEpreuve INT," +
+            "PRIMARY KEY (idAthlete, idEpreuve)," +
+            "FOREIGN KEY (idAthlete) REFERENCES Athlete(idAthlete)," +
+            "FOREIGN KEY (idEpreuve) REFERENCES Epreuve(idEpreuve)" +
+            ");",
+    
+            "CREATE TABLE ParticipeCollectif (" +
+            "idEquipe INT," +
+            "idEpreuve INT," +
+            "PRIMARY KEY (idEquipe, idEpreuve)," +
+            "FOREIGN KEY (idEquipe) REFERENCES Equipe(idEquipe)," +
+            "FOREIGN KEY (idEpreuve) REFERENCES Epreuve(idEpreuve)" +
             ");"
         };
     
