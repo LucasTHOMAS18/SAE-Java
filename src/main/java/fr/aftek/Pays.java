c38a0b47530c53066aa6f4eb305736a9bc1d14e4
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-06-13 14:54:03 +0200

    Doc de pays :3

--- src/main/java/fr/aftek/Pays.java
+++ src/main/java/fr/aftek/Pays.java
@@ -12,32 +12,63 @@ public class Pays {
     private List<Athlete> athletes;
     private List<Equipe> equipes;
 
+    /**
+     * Constructeur de la classe Pays
+     * @param nom le nom du pays
+     */
     public Pays(String nom) {
         this.nom = nom;
         this.athletes = new ArrayList<>();
     }
 
+    /**
+     * Constructeur de la classe Pays
+     * @param nom le nom du pays
+     * @param athletes la liste des athlètes du pays
+     */
     public Pays(String nom, List<Athlete> athletes) {
         this.nom = nom;
         this.athletes = athletes;
     }
 
+    /**
+     * Constructeur de la classe Pays
+     * @param nom le nom du pays
+     * @param athletes la liste des athlètes du pays
+     * @param equipes la liste des équipes du pays
+     */
     public void ajouteAthlete(Athlete athlete) {
         this.athletes.add(athlete);
     }
 
+    /**
+     * Ajoute une équipe à la liste des équipes du pays
+     * @param equipe l'équipe à ajouter
+     */
     public void ajouteEquipe(Equipe equipe) {
         this.equipes.add(equipe);
     }
 
+    /**
+     * Retourne le nom du pays
+     * @return le nom du pays
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Retourne la liste des athlètes du pays
+     * @return la liste des athlètes du pays
+     */
     public List<Athlete> getAthletes() {
         return athletes;
     }
 
+    /**
+     * Retourne la liste des équipes du pays
+     * @return la liste des équipes du pays
+     */
     public List<Equipe> getEquipes() {
         return equipes;
     }
