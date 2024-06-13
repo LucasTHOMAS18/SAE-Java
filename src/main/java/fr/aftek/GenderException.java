4a10e33e553b57e66fafe205b960f03373535923
Rickeur <raphael.cochet1234@outlook.fr>
2024-06-13 14:41:36 +0200

    ajout de doc Epreuve et GenderException

--- src/main/java/fr/aftek/GenderException.java
+++ src/main/java/fr/aftek/GenderException.java
@@ -1,6 +1,11 @@
 package fr.aftek;
 
 public class GenderException extends RuntimeException {
+    /**
+     * Constructeur pour créer une exception avec un message personnalisé.
+     *
+     * @param message Le message de l'exception.
+     */
     public GenderException(String message){
         super(message);
     }
