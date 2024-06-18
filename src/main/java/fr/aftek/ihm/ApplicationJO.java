46d6b272080fc679308aa6e57be6ddee3bcd5006
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-06-18 15:09:32 +0200

    ConnexionMySQL

--- src/main/java/fr/aftek/ihm/ApplicationJO.java
+++ src/main/java/fr/aftek/ihm/ApplicationJO.java
@@ -1,62 +1,31 @@
 package fr.aftek.ihm;
 
-import fr.aftek.ihm.providers.BackgroundsProvider;
-import fr.aftek.ihm.providers.ImagesProvider;
+import fr.aftek.data.ConnexionMySQL;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
 public class ApplicationJO extends Application{
     private Scene scene;
-    private BorderPane root;
+    private ConnexionMySQL connexion;
 
-    @Override
-    public void init() throws Exception {
-        ImagesProvider.loadImages(getClass().getClassLoader().getResource("images").toURI());
-        BackgroundsProvider.loadBackgrounds(getClass().getClassLoader().getResource("backgrounds.json").toURI());
-    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Création d'une instance de FXMLLoader
-        FXMLLoader loader = new FXMLLoader (getClass().getResource("/PageAdmin.fxml"));
-        
-        // Définir ce contrôleur comme celui utilisé par le fichier FXML
-        loader.setController(this);
-        
-        // Chargement du fichier FXML
-        BorderPane root = loader.load();
+        // Connexion à la base de données
+        this.connexion = new ConnexionMySQL();
 
         // Création de la scène
-        Scene scene = new Scene(root, 800, 600);
+        BorderPane root = new PageConnection(connexion);
+        this.scene = new Scene(root, 800, 600);
         stage.setScene(scene);
         stage.setTitle("Jeux IUT'Olympiques");
         stage.setResizable(false);
         stage.show();
     }
-
-    private void initRoot(){
-        if(this.root == null){
-            this.root = new BorderPane();
-        }else{
-            this.root.getChildren().clear();
-        }
-        this.root.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-        this.root.setTop(new Titre());
-    }
-
-    @Override
-    public void stop() throws Exception {
-        
-    }
+    
     public static void main(String[] args) {
         launch(ApplicationJO.class);
     }
---
+++ src/main/java/fr/aftek/ihm/PageConnection.java
@@ -0,0 +1,55 @@
+package fr.aftek.ihm;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import fr.aftek.data.ConnexionMySQL;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class PageConnection extends BorderPane {
+    private ConnexionMySQL connexion;
+   
+    @FXML
+    private TextField identifiantTxtField;
+
+    @FXML
+    private PasswordField mdpPwrdField;
+    
+    @FXML
+    private Button btnConnection;
+    
+    public PageConnection(ConnexionMySQL connexion) throws IOException {
+        this.connexion = connexion;
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/PageAccueil.fxml"));
+        loader.setController(this);
+        loader.setRoot(this);
+        loader.load();
+    }
+    
+
+    public void seConnecter() throws SQLException {
+        String identifiant = identifiantTxtField.getText();
+        String mdp = mdpPwrdField.getText();
+        String serveur = "servinfo-maria";
+        String nomBDD = "DB" + identifiant; 
+
+        try {
+            connexion.connecter(serveur, nomBDD, identifiant, mdp);
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion à la base de données");
+            System.out.println(e.getMessage());
+        }
+
+        if (connexion.isConnecte()) {
+            System.out.println("Connexion réussie");
+        } else {
+            System.out.println("Connexion échouée");
+        }
+    }
+}
