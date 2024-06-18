adc17e5fe7a1cf18539b534a7feed62044e83dcb
YasNeko <107752564+YasNeko@users.noreply.github.com>
2024-06-18 17:50:55 +0200

    Séparation de la vue et du controleur

---
+++ src/main/java/fr/aftek/ihm/ControleurConnexion.java
@@ -0,0 +1,54 @@
+package fr.aftek.ihm;
+
+import java.sql.SQLException;
+import fr.aftek.data.ConnexionMySQL;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class ControleurConnexion {
+    private ConnexionMySQL connexion;
+
+    @FXML
+    private TextField identifiantTxtField;
+
+    @FXML
+    private PasswordField mdpPwrdField;
+
+    @FXML
+    private Button btnConnection;
+
+    public ControleurConnexion(ConnexionMySQL connexion) {
+        this.connexion = connexion;
+    }
+
+    @FXML
+    public void seConnecter() {
+        String identifiant = identifiantTxtField.getText();
+        String mdp = mdpPwrdField.getText();
+        String serveur = "servinfo-maria";
+        String nomBDD = "DB" + identifiant; 
+
+        try {
+            connexion.connecter(serveur, nomBDD, identifiant, mdp);
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion à la base de données");
+            System.out.println(e.getMessage());
+        }
+
+        if (connexion.isConnecte()) {
+            System.out.println("Connexion réussie");
+        } else {
+            System.out.println("Connexion échouée");
+        }
+    }
+
+    @FXML
+    public void mdpKeyPressed(KeyEvent event) {
+        if (event.getCode().toString().equals("ENTER")) {
+            seConnecter();
+        }
+    }
+}
--- src/main/java/fr/aftek/ihm/PageConnection.java
+++ src/main/java/fr/aftek/ihm/PageConnection.java
@@ -1,64 +1,19 @@
 package fr.aftek.ihm;
 
 import java.io.IOException;
-import java.sql.SQLException;
-
 import fr.aftek.data.ConnexionMySQL;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 
 public class PageConnection extends BorderPane {
     private ConnexionMySQL connexion;
-    
-    @FXML
-    private TextField identifiantTxtField;
 
-    @FXML
-    private PasswordField mdpPwrdField;
-    
-    @FXML
-    private Button btnConnection;
-    
     public PageConnection(ConnexionMySQL connexion) throws IOException {
         this.connexion = connexion;
         
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/PageAccueil.fxml"));
-        loader.setController(this);
+        loader.setController(new ControleurConnexion(connexion));
         loader.setRoot(this);
         loader.load();
-    }
-
-    @FXML
-    public void seConnecter() {
-        String identifiant = identifiantTxtField.getText();
-        String mdp = mdpPwrdField.getText();
-        String serveur = "servinfo-maria";
-        String nomBDD = "DB" + identifiant; 
-
-        try {
-            connexion.connecter(serveur, nomBDD, identifiant, mdp);
-        } catch (SQLException e) {
-            System.out.println("Erreur de connexion à la base de données");
-            System.out.println(e.getMessage());
-        }
-
-        if (connexion.isConnecte()) {
-            System.out.println("Connexion réussie");
-            
-        } else {
-            System.out.println("Connexion échouée");
-        }
-    }
-
-    @FXML
-    public void mdpKeyPressed(KeyEvent event) {
-        if (event.getCode().toString().equals("ENTER")) {
-            seConnecter();
-        }
     }
 }
