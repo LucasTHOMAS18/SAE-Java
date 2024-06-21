547c7868a55ce48f2d8d65a3ccc6ace02c8f012b
Tony Beaujouan <tony.beaujouan2@gmail.com>
2024-06-21 10:04:59 +0200

    Merge

--- src/main/java/fr/aftek/ihm/controleurs/ControleurModifEpreuve.java
+++ src/main/java/fr/aftek/ihm/controleurs/ControleurModifEpreuve.java
@@ -34,6 +34,8 @@ public class ControleurModifEpreuve extends Controleur {
     private TableColumn<EpreuveLigne, Character> sexe;
     @FXML
     private TableColumn<EpreuveLigne, Integer> nbAthletes;
+    private Collection<Epreuve> epreuves;
+    private Map<EpreuveLigne, Epreuve> epreuvesMap = new HashMap<>();
 
     public ControleurModifEpreuve(ApplicationJO application) {
         this.application = application;
@@ -61,24 +63,125 @@ public class ControleurModifEpreuve extends Controleur {
                 EpreuveLigne el = table.getSelectionModel().getSelectedItem();
                 Epreuve e = ApplicationJO.PROVIDER.getManager().getEpreuve(el.getNom(), el.getSport());
                 e.setNom(popUp.getTf().getText());
-                init(liste);
+                init(epreuves);
             }
         } 
     }
 
     public void modifierSport(){
-
+        if(selectionner()){
+            PopUp<String> popUp = new PopUp<>(PopUpType.DEMANDER, "Modifier sport", "Quel sport voulez-vous attribuer à cette Epreuve?");
+            popUp.showAndWait();
+            if(!popUp.getTf().getText().isBlank()){
+                EpreuveLigne el = table.getSelectionModel().getSelectedItem();
+                Epreuve e = ApplicationJO.PROVIDER.getManager().getEpreuve(el.getNom(), el.getSport());
+                Sport s = ApplicationJO.PROVIDER.getManager().getSport(popUp.getTf().getText());
+                if (s == null){
+                    popUp = new PopUp<>(PopUpType.ERREUR, "Erreur", "Ce sport n'existe pas");
+                    popUp.showAndWait();
+                    return;
+                }
+                e.setSport(s);
+                init(epreuves);
+            }
+        }
     }
-    public void modifierSexe(){
-
+    
+    public void modifierSexe() {
+        if (selectionner()) {
+            PopUp<String> popUp = new PopUp<>(PopUpType.DEMANDER, "Modifier sexe", "Quel sexe voulez-vous attribuer à cette Epreuve? (H/F)");
+            popUp.showAndWait();
+            if (!popUp.getTf().getText().isBlank()) {
+                EpreuveLigne el = table.getSelectionModel().getSelectedItem();
+                Epreuve e = ApplicationJO.PROVIDER.getManager().getEpreuve(el.getNom(), el.getSport());
+                char sexe = popUp.getTf().getText().charAt(0);
+                if (sexe == 'H' || sexe == 'F') {
+                    e.setSexe(sexe);
+                    init(epreuves);
+                } else {
+                    popUp = new PopUp<>(PopUpType.ERREUR, "Erreur", "Sexe invalide. Veuillez entrer 'H' pour Homme ou 'F' pour Femme.");
+                    popUp.showAndWait();
+                }
+            }
+        }
     }
     public void ajouterAthlete(){
-
+        if(selectionner()){
+            final ApplicationJO application = this.application;
+            final Collection<Athlete> li = new ArrayList<Athlete>();
+            Epreuve e = epreuvesMap.get(table.getSelectionModel().getSelectedItem());
+            for(Athlete a : ApplicationJO.PROVIDER.getManager().getAthletes()){
+                if(!e.getParticipants().contains(a)){
+                    li.add(a);
+                }
+            }
+            final Collection<Epreuve> listeMax = this.epreuves;
+            Task<PageSelectionAthlete> task = new Task<PageSelectionAthlete>() {
+                protected PageSelectionAthlete call() throws Exception {
+                    return new PageSelectionAthlete(application, li, true, (liste) -> {
+                        for(Athlete a : liste){
+                            Epreuve e = epreuvesMap.get(table.getSelectionModel().getSelectedItem());
+                            e.ajouteAthlete(a);
+                            a.ajouteEpreuve(e);
+                        }
+                        init(listeMax);
+                    });
+                };
+            };
+            application.afficherPage(task, "Création des athlètes", "Veuillez patienter...");
+        }
     }
     public void supprimerAthlete(){
-        
+        if(selectionner()){
+            final ApplicationJO application = this.application;
+            final Collection<Athlete> li = new ArrayList<Athlete>();
+            Epreuve e = epreuvesMap.get(table.getSelectionModel().getSelectedItem());
+            for(Athlete a : ApplicationJO.PROVIDER.getManager().getAthletes()){
+                if(e.getParticipants().contains(a)){
+                    li.add(a);
+                }
+            }
+            final Collection<Epreuve> listeMax = this.epreuves;
+            Task<PageSelectionAthlete> task = new Task<PageSelectionAthlete>() {
+                protected PageSelectionAthlete call() throws Exception {
+                    return new PageSelectionAthlete(application, li, true, (liste) -> {
+                        for(Athlete a : liste){
+                            Epreuve e = epreuvesMap.get(table.getSelectionModel().getSelectedItem());
+                            e.retireAthlete(a);
+                            a.retireEpreuve(e);
+                        }
+                        init(listeMax);
+                    });
+                };
+            };
+            application.afficherPage(task, "Création des athlètes", "Veuillez patienter...");
+        }
     }
     public void supprimerEpreuve(){
-
+        if(selectionner()){
+            Epreuve e = epreuvesMap.get(table.getSelectionModel().getSelectedItem());
+            Optional<ButtonType> res = new PopUp<ButtonType>(PopUpType.CONFIRMATION, "Attention !", "Voulez-vous vraiment supprimer cette épreuve?").showAndWait();
+            res.ifPresent((bt)->{
+                if(bt == ButtonType.OK){
+                    System.out.println("supprimer");
+                    for(Athlete a : e.getParticipants()){
+                        a.retireEpreuve(e);
+                    }
+                    System.out.println(ApplicationJO.PROVIDER.getManager().getEpreuves().size());
+                    ApplicationJO.PROVIDER.getManager().getEpreuves().remove(e);
+                    epreuves.remove(e);
+                    System.out.println(ApplicationJO.PROVIDER.getManager().getEpreuves().size());
+                    init(epreuves);
+                }
+            });
+        }
+    }
+    private boolean selectionner(){
+        EpreuveLigne el = table.getSelectionModel().getSelectedItem();
+        if(el == null){
+            new PopUp<>(PopUpType.INFORMATION, "Erreur", "Veuillez sélectionner une épreuve").showAndWait();
+            return false;
+        }
+        return true;
     }
 }
